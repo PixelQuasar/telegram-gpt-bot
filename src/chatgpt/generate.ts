b1bc214ecf41adf6447f1d87b2848e371a93b661
PixelQuasar <max281005@gmail.com>
2023-07-03 00:51:45 +0600

    fix

--- src/chatgpt/generate.ts
+++ src/chatgpt/generate.ts
@@ -3,7 +3,7 @@ import { OpenAIApi } from "openai"
 const generateMessage = async (openai: OpenAIApi, message: string): Promise<string | undefined> => {
         const chatCompletion = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
-            messages: [{role: "user", content: message}, {role: "system", content: "Диалог идет в телеграм-беседе"}],
+            messages: [{role: "user", content: message}, {role: "system", content: "Диалог идет в телеграм-беседе, тебе нужно продолжить его."}],
         })
 
         return chatCompletion.data.choices[0].message?.content
